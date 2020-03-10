6de17b0318c7a6a5006a47ece8f70f5560ac9167
yewonblairseo@gmail.com <yewonblairseo@gmail.com>
2020-03-10 15:38:13 +0100

    building printer

--- Encapsulation/Machine_Printer.py
+++ Encapsulation/Machine_Printer.py
@@ -15,33 +15,14 @@ class Machine:
             self.__power = power
         else:
             print("There is no option other than on or off")
-    
-
-class Paper:
-
-    def __init__(self):
-        self.__amount = 10
-    
-    @property
-    def amount(self):
-        return self.amount
-    
-    @amount.setter
-    def amount(self, quantity):
-        self.__amount = self.__amount - quantity
 
     
-class Papertray(Paper):
+class Papertray():
 
-    def __init__(self):
-        super().__init(paper)
+    def __init__(self, papers):
         self.__isEmpty = False
-        self.__papers = 3
-    
-    @property
-    def paper(self):
-        return self._Paper__amount
-    
+        self.__papers = papers
+
     @property
     def isEmpty(self):
         return self.__isEmpty
@@ -50,14 +31,9 @@ class Papertray(Paper):
     def papers(self):
         return self.__papers
 
-    @paper.setter
-    def paper(self, quantity):
-        self._Paper__amount(quantity)
-
     @isEmpty.setter
     def isEmpty(self, empty):
-        if paper == 0:
-            pass
+        self.__isEmpty = empty
 
     @papers.setter
     def papers(self, papersUsed):
@@ -71,23 +47,23 @@ class Papertray(Paper):
         if self.__papers > 0:
             pass
         else:
-            paper = Paper()
-            paper.amount = paper.amount(paper, 3)
-            self.papers = 3
+            self.__papers += 10
 
 
-class Printer(Machine, Papertray):
+class Printer(Machine):
     
     def __init__(self, machine, papertray):
         # If you inherit one class, you can do it this way
         # super().__init(model)
         # If you inherit multiple classes, you can do this way
         # Machine.__init__(self, model):
+        papers = Papertray(10)
         Machine.__init__(self, machine)
         Papertray.__init__(self, papertray)
 
     if __name__ == "__main__":
-        print(f"There are {Papertray.papers} papers on the paper tray.")
+        printer = Printer()
+        print(f"There are {printer.papers} papers on the paper tray.")
         print(f"If you want to proceed, turn on the machine.")
         userInput = input("Type in 'on' to turn on the machine: ")
         if userInput.lower() == 'on':
@@ -97,9 +73,8 @@ class Printer(Machine, Papertray):
         newInput = input("Type in 'off' to turn off the machine. Type 'print' to print: ")
         while (newInput.lower() != 'off'):
             if newInput == 'print':
-                papertray = Papertray()
-                Papertray.papers(1)
-                print(f"Print completed. {Papertray.amount} papers left on the paper tray.")
+                printer.papers.papers(1)
+                print(f"Print completed. {papers.papers} papers left on the paper tray.")
             else:
                 print("Invalid access. Try again.")
 
